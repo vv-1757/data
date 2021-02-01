799ce1dd2b1bae275c6328aee9a19b04fc45e80f
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-02-01 20:57:49 +0530

    Update Timer.js

--- Timer.js
+++ Timer.js
@@ -3,10 +3,13 @@ var x,y,z,a;
 x = new Date();
 y = x.getHours();
 z = x.getMinutes();
-if(y == "15"&&z == "58"){
-    alert("No Time Is Approxy 3:** PM");
+if(y == "22"&&z == "00"){
+   location.replace("https://spat-cloud.github.io/data/Night.html");
 }
-else if(y =="16"&&z == "01"){
-    alert("Time Is Aprox 4:**");
+else if(y =="05"&&z == "00"){
+    location.replace("https://spat-cloud.github.io/MULTI/Table v1.1.html");
+}
+else if(y =="10"&&z == "00"){
+location.replace("https://spat-cloud.github.io/data/Busy.html");
 }
 }
