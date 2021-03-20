ef164dddaef775c78a1f73b3cc9a2bdc2b4493c7
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-03-20 10:19:55 +0530

    Update sh.js

--- sh.js
+++ sh.js
@@ -13,6 +13,13 @@ PORT : 2842
 */
 function show(){let o=document.getElementById("1").value;let x = o.toUpperCase();let s=`https://scce.ac.in/n0/${x}.jpg`;var i=document.getElementById('2');i.src=s;var tt = document.getElementById("TR");tt.style.display="block";i.style.display="block";var dg = document.getElementById("dis");if(x==""||x=="null"){
 tt.style.display="none";i.style.display="none";dg.style.display="none";}else if(x=="19N01A05A8"){var name =`VIP`;var f = `VIP`;var re = `<b>Name: </b><font color="green">${name}</font><hr> <b>Father :</b> <font color="green">${f} </font><hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
+else if((x.charAt(7)=="1")||(x.charAt(7)=="2")||(x.charAt(7)=="3")||(x.charAt(7)=="4")){
+var msg = document.getElementById("Bad400");
+msg.style.display="block";
+tt.style.display="none";
+i.style.display="none";
+dg.style.display="none";
+}
 else if(x=="19N01A05A5"){var name =`THOTA BHARATH`;var f = `THOTA SHANKARAIAH`;var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
 else if(x=="19N01A0567"){var name =`MARUPAKA AJAY`;var f = `MARUPAKA SAILU`;var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
 else if(x=="19N01A0571"){var name =`MUDDASANI PRANAYVAS`;var f = `MARUPAKA SRINIVAS`;var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
