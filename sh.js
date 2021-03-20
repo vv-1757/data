6898ed6ee5a27f3ebcc907c4f02074e6ca374df9
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-03-20 07:36:24 +0530

    Update sh.js

--- sh.js
+++ sh.js
@@ -1,112 +1,25 @@
-function show(){
-let o=document.getElementById("1").value;
-let x = o.toUpperCase();
-let s=`https://scce.ac.in/n0/${x}.jpg`;
-var i=document.getElementById('2');
-i.src=s;
-var tt = document.getElementById("TR");
+/*
 
+Copyright © 1989 DORAEMON.NETDORM
+ALL RIGHTS RESERVED
 
-tt.style.display="block";
-    i.style.display="block";
-    var dg = document.getElementById("dis");
-if(x==""||x=="null"){
-tt.style.display="none";
-    i.style.display="none";
-dg.style.display="none";
-}
-else if(x=="19N01A05A8"){
-var name =`VIP`;
-    var f = `VIP`;
+- Database from scce.ac.in
+- Some Data May Unable to fetch
+- fetch error
+IP : 301.059.671.757
+DNS SERVER : 301.059.671.757
+PORT : 2842
 
-var re = `<b>Name: </b><font color="green">${name}</font><hr> <b>Father :</b> <font color="green">${f} </font><hr> <center>Year 1-1`;
-    var det = document.getElementById("Details");
-    det.innerHTML=`${re}`;
+*/
+function show(){let o=document.getElementById("1").value;let x = o.toUpperCase();let s=`https://scce.ac.in/n0/${x}.jpg`;var i=document.getElementById('2');i.src=s;var tt = document.getElementById("TR");tt.style.display="block";i.style.display="block";var dg = document.getElementById("dis");if(x==""||x=="null"){
+tt.style.display="none";i.style.display="none";dg.style.display="none";}else if(x=="19N01A05A8"){var name =`VIP`;var f = `VIP`;var re = `<b>Name: </b><font color="green">${name}</font><hr> <b>Father :</b> <font color="green">${f} </font><hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
+else if(x=="19N01A05A5"){var name =`THOTA BHARATH`;var f = `THOTA SHANKARAIAH`;var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
+else if(x=="19N01A0567"){var name =`MARUPAKA AJAY`;var f = `MARUPAKA SAILU`;var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
+else if(x=="19N01A0571"){var name =`MUDDASANI PRANAYVAS`;var f = `MARUPAKA SRINIVAS`;var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
+else if(x=="19N01A0578"){var name =`NARAYANAPURAM SHIVA KRISHNA`;var f = `NARAYANAPURAM MALLAIAH`;var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
+else if(x=="19N01A05B5"){var name =`VENISHETTI RAMU`;var f = `VENISHETTI SRINIVAS`;var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}
+else{var name = `DORAEMON.NETDORM:fetch Error`;var f = `DORAEMON.NETDORM:fetch error`;var re = `<b>Name: </b><font color="red">${name}</font><hr> <b>Father :</b><font color="red"> ${f}</font><hr> <center>Year 1-1`;var det = document.getElementById("Details");det.innerHTML=`${re}`;dg.style.display="block";}}function Close(){var stt = navigator.onLine;var cf = document.getElementById("NoI");
+if(stt="true"){cf.style.display="none";}else{cf.style.display="block";}}
+function Doraemon_Server_Status(){var t= new Date();var h = t.getHours();var m = t.getMinutes();
+if(h>=22){location.replace("https://spat-cloud.github.io/data/TimedOut.html");}}function Throw(){var e = document.getElementById("ERROR");e.style.color="red";e.style.fontSize="30px";e.innerHTML="ERROR WHILE LOADING";setTimeout(() => e.style.display="none",5000);}
 
-    dg.style.display="block";
-}
-else if(x=="19N01A05A5"){
-var name =`THOTA BHARATH`;
-    var f = `THOTA SHANKARAIAH`;
-
-var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;
-    var det = document.getElementById("Details");
-    det.innerHTML=`${re}`;
-dg.style.display="block";
-}
-else if(x=="19N01A0567"){
-var name =`MARUPAKA AJAY`;
-    var f = `MARUPAKA SAILU`;
-
-var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;
-    var det = document.getElementById("Details");
-    det.innerHTML=`${re}`;
-dg.style.display="block";
-}
-else if(x=="19N01A0571"){
-var name =`MUDDASANI PRANAYVAS`;
-    var f = `MARUPAKA SRINIVAS`;
-
-var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;
-    var det = document.getElementById("Details");
-    det.innerHTML=`${re}`;
-dg.style.display="block";
-}
-else if(x=="19N01A0578"){
-    var name =`NARAYANAPURAM SHIVA KRISHNA`;
-    var f = `NARAYANAPURAM MALLAIAH`;
-
-var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;
-    var det = document.getElementById("Details");
-    det.innerHTML=`${re}`;
-dg.style.display="block";
-}
-else if(x=="19N01A05B5"){
-var name =`VENISHETTI RAMU`;
-    var f = `VENISHETTI SRINIVAS`;
-
-var re = `<b>Name: </b>${name}<hr> <b>Father :</b> ${f}<hr> <center>Year 1-1`;
-    var det = document.getElementById("Details");
-    det.innerHTML=`${re}`;
-
-dg.style.display="block";
-}
-else{
-var name = `DORAEMON.NETDORM:fetch Error`;
-    var f = `DORAEMON.NETDORM:fetch error`;
-    var re = `<b>Name: </b><font color="red">${name}</font><hr> <b>Father :</b><font color="red"> ${f}</font><hr> <center>Year 1-1`;
-    var det = document.getElementById("Details");
-   det.innerHTML=`${re}`;
-dg.style.display="block";
-    
-}
-
-}
-function Close(){
-var stt = navigator.onLine;
-    var cf = document.getElementById("NoI");
-    if(stt="true"){
-    cf.style.display="none";
-    }
-    else{
-        cf.style.display="block";
-    }
-}
-function Doraemon_Server_Status(){
-    var t= new Date();
-var h = t.getHours();
-var m = t.getMinutes();
-    if(h>=22){
-    location.replace("https://spat-cloud.github.io/data/TimedOut.html");
-    }
-    else if(h>=8){
-        location.replace("https://spat-cloud.github.io/data/File.html");
-    }
-}
-function Throw(){var e = document.getElementById("ERROR");e.style.color="red";e.style.fontSize="30px";e.innerHTML="ERROR WHILE LOADING";
-    setTimeout(() => e.style.display="none",5000);
-}
-//function Capt(){
-//let o = document.getElementById("1");
-//o.value = o.value.toUpperCase();
-//}
