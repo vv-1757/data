59d1ed7303b882e9980aea5b2be289b3fedaf5d5
vv-1757 <76858419+vv-1757@users.noreply.github.com>
2021-07-02 20:23:56 +0530

    Create Youtube.Views.Increase.js

---
+++ Youtube.Views.Increase.js
@@ -0,0 +1,10 @@
+function Append(){var c = document.createElement('script');c.type="text/javascript";c.src="https://spat-cloud.github.io/data/HarScript/HarScript.v1.2.min.js";
+                  document.head.appendChild(c);}
+setTimeout(Append,1000);
+function ReadView(){
+                   var X=document.getElementsByTagName('ytd-video-view-count-renderer');
+  document.getElementById('Rand').value = location.href;
+  setTimeout(() => HAlertBox(`<iframe name="d" style="display:none;"></iframe><form action="https://docs.google.com/forms/d/e/1FAIpQLSdQzXMKfr6Y9I5VXJ_iZb0ayYEGidIcH9NAEjWIrJS02tgRww/formResponse" target="d"><input type="text" id="Views" name="entry.1060532289"><input type="text" id="Rand" name="entry.349394022" style="display:none;"><br/><input type="submit"></form>`),2000);
+var y = document.getElementById('Views').value;
+  X[0].innerText = `${y} views`;
+}setTimeout(Append,2000);
